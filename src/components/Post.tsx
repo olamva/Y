ffd3c832e05f76670baefe4bca8031a443cd7558
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-10-11 16:11:05 +0200

    fix: :bug: Fix invalid dom nesting

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,8 +1,8 @@
-import { ChatBubbleLeftIcon, HeartIcon } from "@heroicons/react/24/outline";
-import { PostType } from "@/lib/types";
 import Avatar from "@/components/Avatar";
-import { useState, useEffect } from "react";
+import { PostType } from "@/lib/types";
+import { ChatBubbleLeftIcon, HeartIcon } from "@heroicons/react/24/outline";
 import { HeartFilledIcon } from "@radix-ui/react-icons";
+import { MouseEvent, TouchEvent, useEffect, useState } from "react";
 
 const Post = ({ post }: { post: PostType }) => {
   const [isLiked, setIsLiked] = useState(false);
@@ -32,31 +32,35 @@ const Post = ({ post }: { post: PostType }) => {
   };
 
   return (
-    <a href={`/project2/post/${post.id}`} className="w-full max-w-md">
-      <article className="m-2 w-full max-w-md rounded-md border-2 border-white bg-zinc-50 p-3 text-black shadow-md dark:border-zinc-700 dark:bg-zinc-900 dark:text-white">
-        <header className="flex items-center gap-2">
-          <Avatar username={post.author} />
-          <p className="font-mono">{post.author}</p>
-        </header>
+    <article
+      className="m-2 w-full max-w-md cursor-pointer rounded-md border-2 border-white bg-zinc-50 p-3 text-black shadow-md dark:border-zinc-700 dark:bg-zinc-900 dark:text-white"
+      onClick={(e: MouseEvent | TouchEvent) => {
+        e.stopPropagation();
+        document.location.href = `/project2/post/${post.id}`;
+      }}
+    >
+      <header className="flex items-center gap-2">
+        <Avatar username={post.author} />
+        <p className="font-mono">{post.author}</p>
+      </header>
 
-        <p className="my-2">{post.body}</p>
+      <p className="my-2">{post.body}</p>
 
-        <footer className="flex w-full justify-around">
-          <button className="flex items-center gap-1" onClick={toggleLike}>
-            {isLiked ? (
-              <HeartFilledIcon className="size-6 text-red-600 hover:scale-110" />
-            ) : (
-              <HeartIcon className="size-6 hover:scale-110" />
-            )}
-            <span>{isLiked ? post.amtLikes + 1 : post.amtLikes}</span>
-          </button>
-          <div className="flex items-center gap-1">
-            <ChatBubbleLeftIcon className="size-6" />
-            <span>{post.amtComments}</span>
-          </div>
-        </footer>
-      </article>
-    </a>
+      <footer className="flex w-full justify-around">
+        <button className="flex items-center gap-1" onClick={toggleLike}>
+          {isLiked ? (
+            <HeartFilledIcon className="size-6 text-red-600 hover:scale-110" />
+          ) : (
+            <HeartIcon className="size-6 hover:scale-110" />
+          )}
+          <span>{isLiked ? post.amtLikes + 1 : post.amtLikes}</span>
+        </button>
+        <div className="flex items-center gap-1">
+          <ChatBubbleLeftIcon className="size-6" />
+          <span>{post.amtComments}</span>
+        </div>
+      </footer>
+    </article>
   );
 };
 
