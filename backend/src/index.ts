5551e309e94523866369a4ed31923703cc3b552f
fredrir <fhansteen@gmail.com>
2024-10-29 10:27:30 +0100

    change to typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './schema';
+import { resolvers } from './resolvers';
+
+async function startServer() {
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () => console.log(`Server ready at http://localhost:4000${server.graphqlPath}`));
+}
+
+startServer();
