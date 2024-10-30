65de605dd37f281a9ad080303caad8e69621a994
fredrch <fredrik.c.hansteen@ntnu.no>
2024-10-30 16:59:43 +0100

    fix parent id issue

--- frontend/src/queries/comments.ts
+++ frontend/src/queries/comments.ts
@@ -4,6 +4,7 @@ export const GET_COMMENTS = gql`
   query GetComments($postID: ID!) {
     getComments(postID: $postID) {
       id
+      parentID
       body
       author
       amtLikes
@@ -16,6 +17,7 @@ export const GET_COMMENTS_BY_IDS = gql`
   query GetCommentsByIds($ids: [ID!]!) {
     getCommentsByIds(ids: $ids) {
       id
+      parentID
       body
       author
       amtLikes
@@ -28,6 +30,7 @@ export const CREATE_COMMENT = gql`
   mutation CreateComment($body: String!, $parentID: ID!) {
     createComment(body: $body, parentID: $parentID) {
       id
+      parentID
       body
       author
       createdAt
