5648d7270fd3fa28950387359b12d9c682d3406d
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-10-30 18:03:43 +0100

    style: :lipstick: Update styling of navbar

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -6,7 +6,7 @@ const Avatar = ({ username, navbar }: AvatarProps) => (
   <figure>
     <a
       href={`/project2/user${navbar ? "" : "/" + username}`}
-      className="flex size-8 select-none items-center justify-center rounded-full border border-neutral-50 bg-neutral-300 text-center text-gray-900 transition-all hover:scale-105 dark:border-neutral-700 dark:bg-neutral-900 dark:text-white"
+      className="flex size-8 select-none items-center justify-center rounded-full border border-neutral-400 bg-neutral-300 text-center text-gray-900 transition-all hover:scale-105 dark:border-neutral-700 dark:bg-neutral-900 dark:text-white"
     >
       <p>{username[0].toUpperCase()}</p>
     </a>
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -18,51 +18,56 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full bg-gray-950/80 text-white">
-      <div className="flex w-full justify-center">
-        <nav className="flex w-full flex-wrap items-center justify-between px-5 py-5">
-          <a
-            href="/"
-            className="group flex items-center justify-center gap-5 hover:scale-110 hover:text-gray-300"
-          >
-            <h1 className="text-2xl font-semibold sm:text-2xl md:text-2xl lg:text-2xl xl:text-3xl">
-              <span className="group-hover:text-gray-300">Y</span>
-            </h1>
-          </a>
-          <div className="hidden flex-row items-center justify-center gap-2 text-center md:flex">
-            <ThemeToggle />
-            <form onSubmit={navigateSearch}>
-              <input
-                type="search"
-                placeholder="Search here..."
-                className="rounded-md bg-gray-800 p-2 text-white"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-              />
-            </form>
-            <div className="flex flex-row items-center gap-2">
-              {user && <Avatar username={user.username} navbar />}
-              {user ? (
-                <button
-                  onClick={logout}
-                  className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white hover:bg-red-700"
-                >
-                  Logout
-                </button>
-              ) : (
-                <button
-                  onClick={login}
-                  className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white hover:bg-green-700"
-                >
-                  Login
-                </button>
-              )}
+    <>
+      <div className="fixed h-20 w-full bg-gray-200/80 backdrop-blur-sm dark:bg-gray-950/80">
+        <div className="flex w-full justify-center">
+          <nav className="flex w-full flex-wrap items-center justify-between px-5 py-5">
+            <a
+              href="/"
+              className="group flex items-center justify-center gap-5 hover:scale-110 hover:text-gray-300"
+            >
+              <h1 className="text-2xl font-semibold sm:text-2xl md:text-2xl lg:text-2xl xl:text-3xl">
+                <span className="group-hover:text-gray-600 group-hover:dark:text-gray-300">
+                  Y
+                </span>
+              </h1>
+            </a>
+            <div className="hidden flex-row items-center justify-center gap-2 text-center md:flex">
+              <ThemeToggle />
+              <form onSubmit={navigateSearch}>
+                <input
+                  type="search"
+                  placeholder="Search here..."
+                  className="rounded-md bg-gray-100 p-2 outline-none dark:bg-gray-800"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </form>
+              <div className="flex flex-row items-center gap-2">
+                {user && <Avatar username={user.username} navbar />}
+                {user ? (
+                  <button
+                    onClick={logout}
+                    className="rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-700"
+                  >
+                    Log out
+                  </button>
+                ) : (
+                  <button
+                    onClick={login}
+                    className="rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white hover:bg-green-600 dark:bg-green-600 dark:hover:bg-green-700"
+                  >
+                    Log in
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
-          <DropdownMenu />
-        </nav>
+            <DropdownMenu />
+          </nav>
+        </div>
       </div>
-    </div>
+      <div className="h-20"></div>
+    </>
   );
 };
 
