2c52af6b551316b253764cfe8cceb8725bdff77f
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-10-31 14:32:41 +0100

    style: :lipstick: make postwithreply shorter

--- frontend/src/components/Post/PostWithReply.tsx
+++ frontend/src/components/Post/PostWithReply.tsx
@@ -6,19 +6,16 @@ interface PostWithReplyProps {
   post: PostType | CommentType;
   reply: CommentType;
 }
-const PostWithReply = ({ post, reply }: PostWithReplyProps) => {
-  const isComment = "parentID" in post;
-  return (
-    <div className="flex w-full flex-col items-center">
-      {isComment ? (
-        <Comment comment={post} disableBottomMargin />
-      ) : (
-        <Post post={post} disableBottomMargin />
-      )}
-      <div className="h-4 w-1 bg-gray-300 dark:bg-gray-700"></div>
-      <Comment comment={reply} disableTopMargin redirects maxWidth="max-w-lg" />
-    </div>
-  );
-};
+const PostWithReply = ({ post, reply }: PostWithReplyProps) => (
+  <div className="flex w-full flex-col items-center">
+    {"parentID" in post ? (
+      <Comment comment={post} disableBottomMargin />
+    ) : (
+      <Post post={post} disableBottomMargin />
+    )}
+    <div className="h-4 w-1 bg-gray-300 dark:bg-gray-700"></div>
+    <Comment comment={reply} disableTopMargin redirects maxWidth="max-w-lg" />
+  </div>
+);
 
 export default PostWithReply;
