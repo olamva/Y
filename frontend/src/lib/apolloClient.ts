0d27dce0bf2acb0df974ff3a9e55b96cc134d3eb
fredrch <fredrik.c.hansteen@ntnu.no>
2024-11-04 14:37:27 +0100

    revert back to localhost URI for backend

--- frontend/src/lib/apolloClient.ts
+++ frontend/src/lib/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://it2810-06.idi.ntnu.no/graphql",
+  uri: "http://localhost:3001/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
