af2d09dfc266c1d556232a722c5a6bd2ad0ef853
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-11-18 22:14:14 +0100

    fix: :bug: fix bug causing editing to not work

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -205,7 +205,7 @@ const PostPage = () => {
   return (
     <div className="w-full">
       <BackButton
-        overrideRedirect={editing ? `/project2/post/${post!.id}` : "/project2/"}
+        overrideRedirect={editing ? `/project2/post/${post?.id}` : "/project2/"}
       />
       <main className="flex flex-col items-center px-4 pt-5">
         {editing && post ? (
