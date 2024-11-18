3f6f6ef501b5d37b28c5bb432e4cbc60acc82e8e
fredrch <fredrik.c.hansteen@ntnu.no>
2024-11-18 12:28:52 +0100

    styling

--- frontend/src/components/Profile/EditProfile.tsx
+++ frontend/src/components/Profile/EditProfile.tsx
@@ -256,7 +256,7 @@ const EditProfile = ({ user }: Props) => {
                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 sm:text-sm"
                       />
                     </div>
-                    <div className="mb-6">
+                    <div className="my-6">
                       <label
                         htmlFor="profile-picture"
                         className="block text-sm font-medium text-gray-700 dark:text-gray-300"
