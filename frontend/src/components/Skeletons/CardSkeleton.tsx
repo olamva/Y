fad8377272b445014fd7abd7b05a1a93fb99a9e2
fredrch <fredrik.c.hansteen@ntnu.no>
2024-11-18 17:10:21 +0100

    tweak CardSkeleton

--- frontend/src/components/Skeletons/CardSkeleton.tsx
+++ frontend/src/components/Skeletons/CardSkeleton.tsx
@@ -2,8 +2,11 @@ const CardSkeleton = () => {
   return (
     <div className="relative overflow-hidden rounded-lg bg-white p-4 shadow dark:bg-gray-900">
       <div className="absolute inset-0 animate-pulse bg-white blur-sm dark:bg-gray-900"></div>
-      <div className="relative flex h-20 w-52 items-center justify-center">
-        <div className="h-6 w-24 animate-pulse rounded bg-gray-300 dark:bg-gray-700"></div>
+      <div className="relative flex h-10 w-52 items-center justify-center">
+        <div className="h-3 w-24 animate-pulse rounded bg-gray-300 dark:bg-gray-700"></div>
+      </div>
+      <div className="relative flex h-10 w-52 items-center justify-center">
+        <div className="h-3 w-24 animate-pulse rounded bg-gray-300 dark:bg-gray-700"></div>
       </div>
     </div>
   );
