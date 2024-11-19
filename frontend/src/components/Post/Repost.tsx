f1d61f69fa1bf208c721ef0e5d0861b313f7c068
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-11-19 19:12:27 +0100

    style: :lipstick: dark mode coloring of repost

--- frontend/src/components/Post/Repost.tsx
+++ frontend/src/components/Post/Repost.tsx
@@ -22,8 +22,8 @@ const Repost = ({ repost }: { repost: RepostType }) => {
   return (
     <div className="flex flex-col">
       <header className="mx-1 flex h-fit items-end">
-        <div className="h-6 w-4 rounded-tl-lg border-b-0 border-l border-t border-gray-400"></div>
-        <div className="flex items-center gap-2 rounded rounded-b-none bg-gray-200/70 shadow-lg p-2 ">
+        <div className="h-6 w-4 rounded-tl-lg border-b-0 border-l border-t border-gray-300 dark:border-gray-700"></div>
+        <div className="flex items-center gap-2 rounded rounded-b-none bg-gray-200/70 dark:bg-gray-900/80 shadow-lg p-2 ">
           <Avatar user={repost.author} />
           <a
             href={`/project2/user/${repost.author.username}`}
