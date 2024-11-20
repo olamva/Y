c86deb4579afce0f7d4ed4ba56242958037e09b7
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-11-20 11:58:21 +0100

    fix: :bug: Add post skeleton to loading parent

--- frontend/src/components/Post/PostWithReply.tsx
+++ frontend/src/components/Post/PostWithReply.tsx
@@ -2,20 +2,27 @@ import Comment from "@/components/Post/Comment";
 import DeletedPost from "@/components/Post/DeletedPost";
 import Post from "@/components/Post/Post";
 import { CommentType, PostType } from "@/lib/types";
+import PostSkeleton from "../Skeletons/PostSkeleton";
 
 interface PostWithReplyProps {
   post: PostType | CommentType | undefined;
   reply: CommentType;
   replyDoesntRedirect?: boolean;
+  parentsLoading?: boolean;
 }
 const PostWithReply = ({
   post,
   reply,
   replyDoesntRedirect = false,
+  parentsLoading = false,
 }: PostWithReplyProps) => (
   <div className="flex w-full flex-col items-center">
     {!post ? (
-      <DeletedPost />
+      parentsLoading ? (
+        <PostSkeleton />
+      ) : (
+        <DeletedPost />
+      )
     ) : "parentID" in post ? (
       <Comment comment={post} disableBottomMargin />
     ) : (
